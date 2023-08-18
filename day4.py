049bb07f2fa0873210bbcaaffa7609894a3d7243
jayant-01 <01agarwal.jayant@gmail.com>
2023-08-18 05:58:44 +0530

    LCM and GCD

---
+++ day4.py
@@ -0,0 +1,15 @@
+# LCM 
+
+def gcd(a,b):
+    if a==0:
+        return b
+    return gcd(b%a,a)
+
+
+def lcm(a,b):
+    return (a*b)//gcd(a,b)
+
+
+if __name__ == '__main__':
+    a,b = map(int,input().split())
+    print(lcm(a,b))
